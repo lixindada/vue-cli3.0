1e2848ad8631d8fe635c4df52a3d76ce19666c57
lixindada <1046194656@qq.com>
2019-03-05 12:46:23 +0800

    1

---
+++ precache-manifest.4971affac6d93f0f8add0e696cf132a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "48af24cd7dae7280ac56",
+    "url": "/dist/js/about.41d636ee.js"
+  },
+  {
+    "revision": "8a89c70d35eed65091cd",
+    "url": "/dist/css/app.d9ae3f27.css"
+  },
+  {
+    "revision": "8a89c70d35eed65091cd",
+    "url": "/dist/js/app.3cfda21c.js"
+  },
+  {
+    "revision": "e6072e3e74990977fa74",
+    "url": "/dist/js/chunk-vendors.0dd95d52.js"
+  },
+  {
+    "revision": "0d37e1186486460ee9051806f4db886b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
